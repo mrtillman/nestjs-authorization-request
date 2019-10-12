751763bbb429f9f9547060cbb44715cde97d24ec
mrtillman <askmrtillman@gmail.com>
2019-10-11 21:01:23 -0400

    set up nestjs

--- api/secure/index.stubs.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  authorization_code: "open sesame",
-  authResponse: {
-    "access_token": "...",
-    "expires_in": 86400,
-    "token_type": "Bearer",
-    "scope": "openid"
-  }
-};
