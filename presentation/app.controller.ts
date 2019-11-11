af95fe1b007392a4e8551db609d387cd41ba8196
mrtillman <askmrtillman@gmail.com>
2019-11-11 15:26:46 -0500

    fetch counters after login

--- presentation/app.controller.ts
+++ presentation/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Query, Render } from '@nestjs/common';
 import SecureService from '../services/secure.service';
+import CountersService from '../services/counters.service';
 import GetTokenUseCase from '../application/GetToken';
+import GetCountersUseCase from '../application/GetCounters';
+import Counter from '../domain/counter';
 
 @Controller()
 export class AppController {
-  constructor(private readonly secureService: SecureService) {}
+  constructor(private readonly secureService: SecureService,
+              private readonly countersService: CountersService){}
 
   @Get()
   @Render('index')
@@ -14,10 +18,13 @@ export class AppController {
   }
 
   @Get('/oauth2/callback')
-  async oauth2Callback(@Query('code') code: string, @Query('state') state: string): Promise<string> {
-    var getToken = new GetTokenUseCase(this.secureService);
+  async oauth2Callback(@Query('code') code: string, @Query('state') state: string): Promise<Array<Counter>> {
+    const getToken = new GetTokenUseCase(this.secureService);
     getToken.code = code;
     getToken.state = state;
-    return await getToken.execute();
+    const token = await getToken.execute();
+    const getCounters = new GetCountersUseCase(this.countersService);
+    getCounters.token = token;
+    return await getCounters.execute();
   }
 }
