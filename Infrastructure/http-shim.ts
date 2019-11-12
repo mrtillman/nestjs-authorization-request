ef7dcff4e2191b9362fd8622fb0a68c420d06111
mrtillman <askmrtillman@gmail.com>
2019-11-12 13:48:52 -0500

    improve abstractions; encapsulate request logic

--- Infrastructure/http-shim.ts
+++ Infrastructure/http-shim.ts
@@ -1,15 +1,15 @@
+import SERVERS from '../Common/servers';
 import * as fetchImport from 'isomorphic-unfetch';
-import { resolve as join } from 'url';
+import AuthorizationRequest from '../Domain/authorization-request';
+import querystring = require('querystring');
 
 const fetch = (fetchImport.default || fetchImport) as typeof fetchImport.default;
 
 export default class HttpShim {
-  public baseUrl: string;
   public token: string;
   
-  async get(path: string) : Promise<Response> {
-    const resourceUri = join(this.baseUrl, path);
-    return await fetch(resourceUri, {
+  async fetchCounters() : Promise<Response> {
+    return await fetch(`${SERVERS.API}/v1/counters`, {
       method: "GET",
       headers: {
         "authorization": `bearer ${this.token}`,
@@ -17,15 +17,22 @@ export default class HttpShim {
     });
   }
 
-  async post(path: string, content: BodyInit) : Promise<Response> {
-    const resourceUri = join(this.baseUrl, path);
-    return await fetch(resourceUri, {
+  async fetchToken(authRequest: AuthorizationRequest) : Promise<Response> {
+    const requestParameters = querystring.stringify({
+      code: authRequest.code,
+      redirect_uri: authRequest.redirectUri,
+      client_id: authRequest.clientId,
+      client_secret: authRequest.clientSecret,
+      scope: authRequest.scope,
+      grant_type: authRequest.grantType,
+    });
+    return await fetch(`${SERVERS.SECURE}/connect/token`, {
       method: "POST",
       headers: {
         "content-type": "application/x-www-form-urlencoded",
         'accept': 'application/json'
       },
-      body: content,
+      body: requestParameters,
     });
   }
 }
