df84e29ea403a36b42e04f2f590bad8eaea77424
mrtillman <askmrtillman@gmail.com>
2019-11-11 20:42:11 -0500

    replace service imports with use cases; fix breaking chages;

--- presentation/app.controller.ts
+++ presentation/app.controller.ts
@@ -1,30 +1,26 @@
 import { Controller, Get, Query, Render } from '@nestjs/common';
-import SecureService from '../services/secure.service';
-import CountersService from '../services/counters.service';
 import GetTokenUseCase from '../application/GetToken';
 import GetCountersUseCase from '../application/GetCounters';
 import Counter from '../domain/counter';
 
 @Controller()
 export class AppController {
-  constructor(private readonly secureService: SecureService,
-              private readonly countersService: CountersService){}
+  constructor(private readonly getToken: GetTokenUseCase,
+              private readonly getCounters: GetCountersUseCase){}
 
   @Get()
   @Render('index')
   root() {
-    const { AuthorizationUrl } = this.secureService;
+    const { AuthorizationUrl } = this.getToken;
     return { AuthorizationUrl };
   }
 
   @Get('/oauth2/callback')
   async oauth2Callback(@Query('code') code: string, @Query('state') state: string): Promise<Array<Counter>> {
-    const getToken = new GetTokenUseCase(this.secureService);
+    const { getToken, getCounters } = this;
     getToken.code = code;
     getToken.state = state;
-    const token = await getToken.execute();
-    const getCounters = new GetCountersUseCase(this.countersService);
-    getCounters.token = token;
+    getCounters.token = await getToken.execute();
     return await getCounters.execute();
   }
 }
