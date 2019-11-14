fd04fddd428decffe9f0b4306cde0ea1ccdb2659
mrtillman <askmrtillman@gmail.com>
2019-11-14 16:39:37 -0500

    add more tests

--- Presentation/app.controller.spec.ts
+++ Presentation/app.controller.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import CountersModule from './counters.module';
 import SecureModule from './secure.module';
+import authUrlRegEx from '../Common/auth-url.reg-exp';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -11,15 +11,24 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       imports: [CountersModule, SecureModule],
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('index', () => {
+    it('should return view model', () => {
+      const model = appController.index();
+      expect(model).toBeDefined();
+    });
+    it('view model should have authorization url', () => {
+      const model = appController.index();
+      expect(model.authorizationUrl).toBeDefined();
+    });
+    it('authorization url should not be malformed', () => {
+      const model = appController.index();
+      const { authorizationUrl } = model;
+      expect(authUrlRegEx.test(authorizationUrl)).toBe(true);
     });
   });
 });
