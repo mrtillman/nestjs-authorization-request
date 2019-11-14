e5e0d6006faca9d1d8e548b74ff653ea38e46b53
mrtillman <askmrtillman@gmail.com>
2019-11-14 16:39:18 -0500

    remove app service

--- Presentation/app.module.ts
+++ Presentation/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import CountersModule from './counters.module';
 import SecureModule from './secure.module';
 
 @Module({
   imports: [CountersModule, SecureModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
