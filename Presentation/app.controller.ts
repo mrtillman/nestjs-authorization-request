ec97fc03f98c76f1f8249b37daf26329ac4fa035
mrtillman <askmrtillman@gmail.com>
2020-02-21 22:08:00 -0500

    incorporate refresh token; add /renew endpoint

--- Presentation/app.controller.ts
+++ Presentation/app.controller.ts
@@ -1,33 +1,54 @@
 import { Controller, Get, Query, Render } from '@nestjs/common';
 import { GetTokenUseCase } from '../Application/get-token.use-case';
 import { GetCountersUseCase } from '../Application/get-counters.use-case';
+import { RenewTokenUseCase } from '../Application/renew-token.use-case';
 import { Counter } from '../Domain/counter';
+import { AuthorizationResponse } from 'Domain/auth-response';
+
+let authResponse: AuthorizationResponse = null;
 
 @Controller()
 export class AppController {
-  constructor(private readonly getToken: GetTokenUseCase,
-              private readonly getCounters: GetCountersUseCase){}
+  constructor(private readonly getTokenUseCase: GetTokenUseCase,
+              private readonly getCountersUseCase: GetCountersUseCase,
+              private readonly renewTokenUseCase: RenewTokenUseCase){}
 
   @Get('/')
   @Render('index')
   index() {
     // 1. Begin Authorization Request
-    return { authorizationUrl: this.getToken.authorizationUrl };
+    return { authorizationUrl: this.getTokenUseCase.authorizationUrl };
   }
 
-  // 2. Authorization Grant
+  // 2. Authorization Grant (inbound)
   @Get('/oauth2/callback')
-  async oauth2Callback(@Query('code') code: string, @Query('state') state: string): Promise<Array<Counter>> {
-    const { getToken, getCounters } = this;
+  async oauth2Callback(@Query('code') code: string, @Query('state') state: string): Promise<any> {
+    const { 
+      getTokenUseCase, 
+      getCountersUseCase, 
+      renewTokenUseCase } = this;
     
-    // 3. Authorization Grant
-    getToken.code = code;
-    getToken.state = state;
+    // 3. Authorization Grant (outbound)
+    getTokenUseCase.code = code;
+    getTokenUseCase.state = state;
     
-    // 4. & 5. Access Token
-    getCounters.token = await getToken.execute();
+    // 4. Access Token (inbound)
+    authResponse = await getTokenUseCase.execute();
+
+    // 5. Access Token (outbound)
+    getCountersUseCase.token = authResponse.accessToken;
     
     // 6. Protected Resource
-    return await getCounters.execute();
+    const counters = await getCountersUseCase.execute();
+
+    renewTokenUseCase.refreshToken = authResponse.refreshToken;
+
+    return counters;
+  }
+
+  @Get('/renew')
+  async renewToken(){
+    if(!authResponse) return;
+    return await this.renewTokenUseCase.execute();
   }
 }
--- Presentation/secure.module.ts
+++ Presentation/secure.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config.module';
 import { SecureService } from '../Services/secure.service';
 import { GetTokenUseCase } from '../Application/get-token.use-case';
+import { RenewTokenUseCase } from '../Application/renew-token.use-case';
 import { HttpShim } from '../Infrastructure/http-shim';
 
 @Module({
   imports: [ConfigModule],
-  providers: [HttpShim, SecureService, GetTokenUseCase],
-  exports: [GetTokenUseCase],
+  providers: [HttpShim, SecureService, GetTokenUseCase, RenewTokenUseCase],
+  exports: [GetTokenUseCase, RenewTokenUseCase],
 })
 export class SecureModule {}
