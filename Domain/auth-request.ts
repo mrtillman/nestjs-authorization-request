f2b9ce47274e10268415a3348d8ac3915185d7d4
mrtillman <askmrtillman@gmail.com>
2020-02-21 22:07:37 -0500

    make fields optional

--- Domain/auth-request.ts
+++ Domain/auth-request.ts
@@ -2,8 +2,9 @@ export class AuthorizationRequest
 {
   public clientId: string;
   public clientSecret: string;
-  public code: string;
+  public code?: string;
   public grantType: string;
-  public redirectUri: string;
-  public scope: string;
+  public refreshToken?: string;
+  public redirectUri?: string;
+  public scope?: string;
 }
