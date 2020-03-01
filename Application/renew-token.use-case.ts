03fc5d0d33bbd269df06942938b6fd64bf4bda9c
mrtillman <askmrtillman@gmail.com>
2020-03-01 02:59:42 -0500

    cache refresh token; tweak /renewtoken endpoint

--- Application/renew-token.use-case.ts
+++ Application/renew-token.use-case.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { SecureService } from '../Services/secure.service';
 import { UseCase } from './use-case.interface';
-import { AuthorizationResponse } from 'Domain/auth-response';
+import { AuthorizationResponse } from '../Domain/auth-response';
+import { CacheService } from '../Services/cache.service';
+
+let refreshTokenKey = "refreshToken";
 
 @Injectable()
 export class RenewTokenUseCase implements UseCase<AuthorizationResponse> {
   
-  constructor(private readonly secureService : SecureService) {}
+  constructor(private readonly secureService : SecureService,
+              private readonly cacheService: CacheService) {}
   
-  public refreshToken: string;
+  public set refreshToken(value) {
+    this.cacheService.setValue(refreshTokenKey, value);
+  }
+  public get refreshToken(): string {
+    return this.cacheService.getValue(refreshTokenKey);
+  };
 
   public async execute(): Promise<AuthorizationResponse> {
     const result = await this.secureService.renewToken(this.refreshToken);
+    this.refreshToken = result.Value.refreshToken;
     return result.Value;
   }
 }
--- Presentation/app.controller.ts
+++ Presentation/app.controller.ts
@@ -1,43 +1,45 @@
-import { Controller, Get, Query, Render } from '@nestjs/common';
-import { GetTokenUseCase } from '../Application/get-token.use-case';
-import { GetCountersUseCase } from '../Application/get-counters.use-case';
-import { RenewTokenUseCase } from '../Application/renew-token.use-case';
-import { Counter } from '../Domain/counter';
-import { AuthorizationResponse } from 'Domain/auth-response';
-
-let authResponse: AuthorizationResponse = null;
+import { Response as RouteResponse } from "express";
+import { Controller, Get, Query, Render, Response } from "@nestjs/common";
+import { GetTokenUseCase } from "../Application/get-token.use-case";
+import { GetCountersUseCase } from "../Application/get-counters.use-case";
+import { RenewTokenUseCase } from "../Application/renew-token.use-case";
+import { AuthorizationResponse } from "Domain/auth-response";
 
 @Controller()
 export class AppController {
-  constructor(private readonly getTokenUseCase: GetTokenUseCase,
-              private readonly getCountersUseCase: GetCountersUseCase,
-              private readonly renewTokenUseCase: RenewTokenUseCase){}
+  constructor(
+    private readonly getTokenUseCase: GetTokenUseCase,
+    private readonly getCountersUseCase: GetCountersUseCase,
+    private readonly renewTokenUseCase: RenewTokenUseCase
+  ) {}
 
-  @Get('/')
-  @Render('index')
+  private readonly authResponseKey: string = "authResponse";
+
+  @Get("/")
+  @Render("index")
   index() {
     // 1. Begin Authorization Request
     return { authorizationUrl: this.getTokenUseCase.authorizationUrl };
   }
 
   // 2. Authorization Grant (inbound)
-  @Get('/oauth2/callback')
-  async oauth2Callback(@Query('code') code: string, @Query('state') state: string): Promise<any> {
-    const { 
-      getTokenUseCase, 
-      getCountersUseCase, 
-      renewTokenUseCase } = this;
-    
+  @Get("/oauth2/callback")
+  async oauth2Callback(
+    @Query("code") code: string,
+    @Query("state") state: string
+  ): Promise<any> {
+    const { getTokenUseCase, getCountersUseCase, renewTokenUseCase } = this;
+
     // 3. Authorization Grant (outbound)
     getTokenUseCase.code = code;
     getTokenUseCase.state = state;
-    
+
     // 4. Access Token (inbound)
-    authResponse = await getTokenUseCase.execute();
+    const authResponse = await getTokenUseCase.execute();
 
     // 5. Access Token (outbound)
     getCountersUseCase.token = authResponse.accessToken;
-    
+
     // 6. Protected Resource
     const counters = await getCountersUseCase.execute();
 
@@ -46,9 +48,13 @@ export class AppController {
     return counters;
   }
 
-  @Get('/renewtoken')
-  async renewToken(){
-    if(!authResponse) return;
-    return await this.renewTokenUseCase.execute();
+  @Get("/renewtoken")
+  async renewToken(@Response() res: RouteResponse): Promise<any> {
+    var authResponse = await this.renewTokenUseCase.execute();
+    if (authResponse) {
+      res.json(authResponse);
+    } else {
+      res.redirect("/");
+    }
   }
 }
