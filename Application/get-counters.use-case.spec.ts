9d6632f9fad46bb189100edb4322a9c5ffe6ad47
mrtillman <askmrtillman@gmail.com>
2020-03-04 00:09:40 -0500

    finalize tests

--- Application/get-counters.use-case.spec.ts
+++ Application/get-counters.use-case.spec.ts
@@ -1,11 +1,47 @@
 import { GetCountersUseCase } from './get-counters.use-case';
+import { Mock } from 'moq.ts';
+import { CountersService } from 'Services/counters.service';
+
+// TODO: consolidate test doubles
+const token = "t0k3n";
+const counters = [
+  {
+    "_id": "5e3691dda2de8e05241a60a5",
+    "name": "alcohol",
+    "value": 0,
+    "skip": 1
+  },
+  {
+    "_id": "5e3691dda2de8e05241a60a6",
+    "name": "tobacco",
+    "value": 0,
+    "skip": 1
+  },
+  {
+    "_id": "5e3691dda2de8e05241a60a7",
+    "name": "firearms",
+    "value": 0,
+    "skip": 1
+  }
+];
+
+const countersService = new Mock<CountersService>();
+countersService.setup(service => service.token).returns(token);
+countersService.setup(service => service.getCounters())
+               .returns(counters);
 
 describe('GetCountersUseCase', () => {
   let useCase : GetCountersUseCase;
-  beforeEach(() => {
-    useCase = new GetCountersUseCase(null);
+  beforeEach(() => {    
+    useCase = new GetCountersUseCase(countersService.object());
   })
-  it('should work', () => {
-    expect(useCase.execute).toBeDefined();
+  it('should get counters', async () => {
+    useCase.token = token;
+    const _counters = await useCase.execute();
+    expect(_counters).toBeDefined();
+  })
+  it('should get token', async () => {
+    const _token = useCase.token;
+    expect(_token).toBeDefined();
   })
 })
