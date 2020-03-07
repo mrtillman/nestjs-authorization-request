00b3b8a24804148c63326433e8d4aec1f4d31404
mrtillman <askmrtillman@gmail.com>
2020-03-07 18:35:10 -0500

    create local utility methods; refactor tests

--- Application/get-token.use-case.spec.ts
+++ Application/get-token.use-case.spec.ts
@@ -7,41 +7,52 @@ import { KEYS } from '../Common/keys.enum';
 import { Result } from '../Common/result';
 import { authResponse, authorizationUrl, code, state } from '../Common/TestDoubles/stubs';
 
-// TODO: use mock factory; clean up tests
+let secureServiceMock: Mock<SecureService>;
+let cacheServiceMock: Mock<CacheService>;
+
+const getUseCase = (
+    secureServiceMock: Mock<SecureService>,
+    cacheServiceMock: Mock<CacheService>
+  ): GetTokenUseCase => {
+  const useCase = new GetTokenUseCase(secureServiceMock.object(), cacheServiceMock.object());
+  useCase.code = code;
+  useCase.state = state;
+  return useCase;
+};
+
+const mockSecureService = (): Mock<SecureService> => {
+  const secureServiceMock = new Mock<SecureService>();
+  secureServiceMock.setup(service => service.getToken(It.IsAny<string>(), It.IsAny<string>()))
+                   .returns(Result.Ok(authResponse))
+                   .setup(service => service.authorizationUrl)
+                   .returns(authorizationUrl);
+  return secureServiceMock;
+}
+
 describe('GetTokenUseCase', () => {
+  beforeEach(() => {
+    secureServiceMock = mockSecureService();
+    cacheServiceMock = new Mock<CacheService>();
+  })
   it('should get authorization response', () => {
-    let useCase : GetTokenUseCase;
-    const secureServiceMock = new Mock<SecureService>();
-    secureServiceMock.setup(service => service.getToken(It.IsAny<string>(), It.IsAny<string>()))
-                    .returns(Result.Ok(authResponse))
-                    .setup(service => service.authorizationUrl)
-                    .returns(authorizationUrl);
-    const cacheServiceMock = new Mock<CacheService>();
     cacheServiceMock.setup(service => service.getValue<AuthorizationResponse>(KEYS.ACCESS_TOKEN))
                     .returns(null)
-                    .setup(service => service.setValue(It.IsAny<KEYS>(), It.IsAny<any>()))
+                    .setup(service => service.setValue(KEYS.ACCESS_TOKEN, It.IsAny<any>()))
                     .returns(null);
-    useCase = new GetTokenUseCase(secureServiceMock.object(), cacheServiceMock.object());
-    useCase.code = code;
-    useCase.state = state;
+    const useCase = getUseCase(secureServiceMock, cacheServiceMock);
+
     const response = useCase.execute();
+    
     expect(response).toBeDefined();
     secureServiceMock.verify(service => service.getToken(It.IsAny<string>(), It.IsAny<string>()));
   })
   it('should get authorization response from cache', () => {
-    let useCase : GetTokenUseCase;
-    const secureServiceMock = new Mock<SecureService>();
-    secureServiceMock.setup(service => service.getToken(It.IsAny<string>(), It.IsAny<string>()))
-                    .returns(Result.Ok(authResponse))
-                    .setup(service => service.authorizationUrl)
-                    .returns(authorizationUrl);
-    const cacheServiceMock = new Mock<CacheService>();
     cacheServiceMock.setup(service => service.getValue<AuthorizationResponse>(KEYS.ACCESS_TOKEN))
                     .returns(authResponse);
-    useCase = new GetTokenUseCase(secureServiceMock.object(), cacheServiceMock.object());
-    useCase.code = code;
-    useCase.state = state;
+    const useCase = getUseCase(secureServiceMock, cacheServiceMock);
+
     const response = useCase.execute();
+
     expect(response).toBeDefined();
   })
 })
