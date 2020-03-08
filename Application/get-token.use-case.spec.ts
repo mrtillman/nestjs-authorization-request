95947fc22e7aec1d6e322a9359e1cd77e3fe71f5
mrtillman <askmrtillman@gmail.com>
2020-03-07 21:34:42 -0500

    improve test coverage; tweak tests

--- Application/get-token.use-case.spec.ts
+++ Application/get-token.use-case.spec.ts
@@ -1,5 +1,5 @@
 import { GetTokenUseCase } from './get-token.use-case';
-import { Mock, It } from 'moq.ts';
+import { Mock, It, Times } from 'moq.ts';
 import { CacheService } from '../Services/cache.service';
 import { SecureService } from '../Services/secure.service';
 import { AuthorizationResponse } from '../Domain/auth-response';
@@ -10,10 +10,7 @@ import { authResponse, authorizationUrl, code, state } from '../Common/TestDoubl
 let secureServiceMock: Mock<SecureService>;
 let cacheServiceMock: Mock<CacheService>;
 
-const getUseCase = (
-    secureServiceMock: Mock<SecureService>,
-    cacheServiceMock: Mock<CacheService>
-  ): GetTokenUseCase => {
+const getUseCase = (): GetTokenUseCase => {
   const useCase = new GetTokenUseCase(secureServiceMock.object(), cacheServiceMock.object());
   useCase.code = code;
   useCase.state = state;
@@ -29,30 +26,35 @@ const mockSecureService = (): Mock<SecureService> => {
   return secureServiceMock;
 }
 
+const called = (callCount: number): Times => (
+  new Times(expected => expected === callCount, `Should be called ${callCount} times`)
+);
+
 describe('GetTokenUseCase', () => {
   beforeEach(() => {
     secureServiceMock = mockSecureService();
     cacheServiceMock = new Mock<CacheService>();
   })
-  it('should get authorization response', () => {
+  it('should get fresh authorization response', async () => {
     cacheServiceMock.setup(service => service.getValue<AuthorizationResponse>(KEYS.ACCESS_TOKEN))
                     .returns(null)
                     .setup(service => service.setValue(KEYS.ACCESS_TOKEN, It.IsAny<any>()))
                     .returns(null);
-    const useCase = getUseCase(secureServiceMock, cacheServiceMock);
+    const useCase = getUseCase();
 
-    const response = useCase.execute();
+    const response = await useCase.execute();
     
     expect(response).toBeDefined();
-    secureServiceMock.verify(service => service.getToken(It.IsAny<string>(), It.IsAny<string>()));
+    secureServiceMock.verify(service => service.getToken(It.IsAny<string>(), It.IsAny<string>()), called(1));
   })
-  it('should get authorization response from cache', () => {
+  it('should get cached authorization response', async () => {
     cacheServiceMock.setup(service => service.getValue<AuthorizationResponse>(KEYS.ACCESS_TOKEN))
                     .returns(authResponse);
-    const useCase = getUseCase(secureServiceMock, cacheServiceMock);
+    const useCase = getUseCase();
 
-    const response = useCase.execute();
+    const response = await useCase.execute();
 
     expect(response).toBeDefined();
+    secureServiceMock.verify(service => service.getToken(It.IsAny<string>(), It.IsAny<string>()), called(0));
   })
 })
--- Application/renew-token.use-case.spec.ts
+++ Application/renew-token.use-case.spec.ts
@@ -1,11 +1,32 @@
+import { Mock, It } from 'moq.ts';
 import { RenewTokenUseCase } from './renew-token.use-case';
+import { SecureService } from '../Services/secure.service';
+import { CacheService } from '../Services/cache.service';
+import { refreshToken } from '../Common/TestDoubles/stubs';
+import { KEYS } from '../Common/keys.enum';
+
+let secureServiceMock: Mock<SecureService>;
+let cacheServiceMock: Mock<CacheService>;
+
+const getUseCase = (): RenewTokenUseCase => {
+  return new RenewTokenUseCase(secureServiceMock.object(), cacheServiceMock.object());
+};
 
 describe('RenewTokenUseCase', () => {
-  let useCase : RenewTokenUseCase;
   beforeEach(() => {
-    useCase = new RenewTokenUseCase(null, null);
+    secureServiceMock = new Mock<SecureService>();
+    cacheServiceMock = new Mock<CacheService>();
   })
-  it('should work', () => {
-    expect(useCase.execute).toBeDefined();
+  it('should return null if no refresh token', async () => {
+    cacheServiceMock
+      .setup(service => service.getValue(KEYS.REFRESH_TOKEN))
+      .returns(null)
+      .setup(service => service.setValue(KEYS.REFRESH_TOKEN, It.IsAny<any>()))
+      .returns(null);
+    const useCase = getUseCase();
+
+    const response = await useCase.execute();
+
+    expect(response).toBeNull();
   })
 })
