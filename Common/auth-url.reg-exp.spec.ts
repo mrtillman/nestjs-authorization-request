e632e07359a6660d5fe8a4603a70ab29886e7d6a
mrtillman <askmrtillman@gmail.com>
2020-03-11 18:30:22 -0400

    improve test coverage

--- Common/auth-url.reg-exp.spec.ts
+++ Common/auth-url.reg-exp.spec.ts
@@ -5,7 +5,12 @@ describe('GetCountersUseCase', () => {
   beforeEach(() => {
     regex = new AuthorizationUrlRegExp();
   })
-  it('should work', () => {
-    expect(regex.test).toBeDefined();
+  it('should reject invalid authorization url', () => {
+    const url = "";
+    expect(regex.test(url)).toBe(false);
+  })
+  it('should accept valid authorization url', () => {
+    const url = "http://localhost:5000/connect/authorize?response_type=code&client_id={client-id}&redirect_uri={redirect_uri}&scope=openid offline_access&state={state}";
+    expect(regex.test(url)).toBe(true);
   })
 })
