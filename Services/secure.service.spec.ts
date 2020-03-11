0e8a30f7502be36ba01735291114ffbbc38d6e77
mrtillman <askmrtillman@gmail.com>
2020-03-11 19:10:59 -0400

    improve test coverage

--- Services/secure.service.spec.ts
+++ Services/secure.service.spec.ts
@@ -1,11 +1,29 @@
 import { SecureService } from './secure.service';
 import { ServiceAgent } from '../Infrastructure/service-agent';
 import { ConfigService } from './config.service';
-import { Mock } from 'moq.ts';
-import { authRequest } from '../Common/TestDoubles/stubs';
+import { Mock, It } from 'moq.ts';
+import { authRequest, authResponse, code, refreshToken } from '../Common/TestDoubles/stubs';
 import { AuthorizationUrlRegExp } from '../Common/auth-url.reg-exp';
+import querystring = require('querystring');
+import { AuthorizationRequest } from '../Domain/auth-request';
 
 const agentMock = new Mock<ServiceAgent>();
+
+const mockTokenResponse = {
+  ok: true,
+  json: () => ({
+    access_token: authResponse.accessToken,
+    expires_in: authResponse.expiresIn,
+    scope: authResponse.scope,
+    token_type: authResponse.tokenType,
+    refresh_toke: authResponse.refreshToken
+  })
+};
+
+agentMock.setup(agent => agent.fetchToken(It.IsAny<AuthorizationRequest>()))
+          .returns(Promise.resolve(Object.assign({}, mockTokenResponse)));
+agentMock.setup(agent => agent.renewToken(It.IsAny<AuthorizationRequest>()))
+          .returns(Promise.resolve(Object.assign({}, mockTokenResponse)));
 
 const configMock = {
   "CLIENT_ID": authRequest.clientId,
@@ -25,5 +43,17 @@ describe('SecureService', () => {
     const isValidAuthUrl = rgx.test(service.authorizationUrl);
 
     expect(isValidAuthUrl).toBe(true);
+  })
+  it('should get token', async () => { 
+    const urlParameters = querystring.parse(service.authorizationUrl);
+    const stateValue = <string>urlParameters.state;
+    
+    const result = await service.getToken(code, stateValue);
+
+    expect(result).toBeDefined();
+  })
+  it('should renew token', async () => { 
+    const result = await service.renewToken(refreshToken);
+    expect(result).toBeDefined();
   })
 })
