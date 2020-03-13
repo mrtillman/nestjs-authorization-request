c5cd4dbe30e4a5a5c1e4be10939dfe07d9d461f8
mrtillman <askmrtillman@gmail.com>
2020-03-12 20:13:27 -0400

    improve test coverage

--- Presentation/app.controller.spec.ts
+++ Presentation/app.controller.spec.ts
@@ -1,23 +1,41 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import querystring = require('querystring');
 import { AppController } from "./app.controller";
-import { CountersModule } from "./counters.module";
-import { SecureModule } from "./secure.module";
 import { AuthorizationUrlRegExp } from "../Common/auth-url.reg-exp";
 import { CacheService } from "../Services/cache.service";
+import { Mock, It } from "moq.ts";
+import { GetTokenUseCase } from "../Application/get-token.use-case";
+import { GetCountersUseCase } from "../Application/get-counters.use-case";
+import { RenewTokenUseCase } from "../Application/renew-token.use-case";
+import { code, counters, authResponse } from "../Common/TestDoubles/stubs";
+import { KEYS } from '../Common/keys.enum';
+import { AuthorizationResponse } from '../Domain/auth-response';
+import { Response } from "express";
+import { called } from '../Common/test-helpers';
+
+const getTokenUseCaseMock = new Mock<GetTokenUseCase>();
+const getCountersUseCaseMock = new Mock<GetCountersUseCase>();
+const cacheMock = new Mock<CacheService>();
+
+let renewTokenUseCaseMock = new Mock<RenewTokenUseCase>();
+let responseMock: Mock<Response> = null;
+
+getTokenUseCaseMock.setup(useCase => useCase.execute()).returns(Promise.resolve(authResponse));
+getCountersUseCaseMock.setup(useCase => useCase.execute()).returns(Promise.resolve(counters));
+renewTokenUseCaseMock.setup(useCase => useCase.execute()).returns(Promise.resolve(authResponse));
+cacheMock.setup(cache => cache.getValue(KEYS.ACCESS_TOKEN)).returns(null);
+cacheMock.setup(cache => cache.setValue(KEYS.ACCESS_TOKEN, It.IsAny<AuthorizationResponse>())).returns(null);
 
 describe("AppController", () => {
   let appController: AppController;
   let authUrlRegExp: AuthorizationUrlRegExp;
 
   beforeEach(async () => {
-    // TODO: mock dependencies
-    const app: TestingModule = await Test.createTestingModule({
-      imports: [CountersModule, SecureModule],
-      providers: [CacheService],
-      controllers: [AppController]
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
+    appController = new AppController(
+      getTokenUseCaseMock.object(),
+      getCountersUseCaseMock.object(),
+      renewTokenUseCaseMock.object(),
+      cacheMock.object()
+    )
     authUrlRegExp = new AuthorizationUrlRegExp();
   });
 
@@ -25,22 +43,47 @@ describe("AppController", () => {
     it("should get authorization url", () => {
       const model = appController.index();
       const isValidAuthUrl = !authUrlRegExp.test(model.authorizationUrl);
-      expect(isValidAuthUrl).toBe(false);
+      expect(isValidAuthUrl).toBe(true);
     });
   });
 
   describe("/oauth2/callback", () => {
-    it("should get counters", () => {
-      expect(true).toBe(true);
+    it("should get counters", async () => {
+      const model = appController.index();
+      const urlParameters = querystring.parse(model.authorizationUrl);
+      const stateValue = <string>urlParameters.state;
+      const _counters = await appController.oauth2Callback(code, stateValue);
+      
+      expect(Array.isArray(_counters)).toBe(true);
+      expect(_counters.length).toBe(3);
     });
   })
 
   describe("/renewtoken", () => {
-    it("should get authorization response", () => {
-      expect(true).toBe(true);
+    beforeEach(() => {
+      responseMock = new Mock<Response>();
+      responseMock.setup(response => response.json(It.IsAny<any>())).returns(null);
+      responseMock.setup(response => response.redirect(It.IsAny<string>())).returns(null);
+    })
+    it("should get authorization response", async () => {
+      await appController.renewToken(responseMock.object());
+
+      responseMock.verify(response => response.json(It.IsAny<any>()), called(1));
+      responseMock.verify(response => response.redirect("/"), called(0));
     });
-    it("should redirect if no authorization response", () => {
-      expect(true).toBe(true);
+    it("should redirect if no authorization response", async () => {
+      renewTokenUseCaseMock = new Mock<RenewTokenUseCase>();
+      renewTokenUseCaseMock.setup(useCase => useCase.execute()).returns(Promise.resolve(null));
+      appController = new AppController(
+        getTokenUseCaseMock.object(),
+        getCountersUseCaseMock.object(),
+        renewTokenUseCaseMock.object(),
+        cacheMock.object()
+      );
+      await appController.renewToken(responseMock.object());
+
+      responseMock.verify(response => response.json(It.IsAny<any>()), called(0));
+      responseMock.verify(response => response.redirect("/"), called(1));
     });
   })
 });
