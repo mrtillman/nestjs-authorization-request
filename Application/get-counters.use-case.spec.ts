59a2753c3e5942c8ef06249280bd8d55b664fcbb
mrtillman <askmrtillman@gmail.com>
2020-03-19 19:47:45 -0400

    run tests in single process; simplify test doubles; revise tests

--- Application/get-counters.use-case.spec.ts
+++ Application/get-counters.use-case.spec.ts
@@ -1,27 +1,25 @@
 import { GetCountersUseCase } from './get-counters.use-case';
 import { Mock } from 'moq.ts';
 import { CounterService } from '../Services/counter.service';
-import { counters, token } from '../Common/test-doubles';
 import { Result } from '../Common/result';
+import { counters, token } from '../Common/test-doubles';
+import { called } from '../Common/test-helpers';
 
 const counterServiceMock = new Mock<CounterService>();
 counterServiceMock.setup(service => service.token)
-                   .returns(token);
+                  .returns(token);
 counterServiceMock.setup(service => service.getCounters())
-                   .returns(Result.Ok(counters));
+                  .returns(Result.Ok(counters));
+const useCase = new GetCountersUseCase(counterServiceMock.object());
 
 describe('GetCountersUseCase', () => {
-  let useCase : GetCountersUseCase;
-  beforeEach(() => {    
-    useCase = new GetCountersUseCase(counterServiceMock.object());
-  })
-  it('should get counters', async () => {
+  it('should get counters', () => {
     useCase.token = token;
-    const _counters = await useCase.execute();
-    expect(_counters).toBeDefined();
+    useCase.execute();
+    counterServiceMock.verify(service => service.getCounters(), called(1));
   })
-  it('should get token', async () => {
-    const _token = useCase.token;
-    expect(_token).toBeDefined();
+  it('should get token', () => {
+    useCase.token;
+    counterServiceMock.verify(service => service.token, called(1));
   })
 })
--- Common/test-doubles.ts
+++ Common/test-doubles.ts
@@ -1,17 +1,15 @@
 import { AuthorizationResponse } from '../Domain/auth-response';
 import { AuthorizationRequest } from '../Domain/auth-request';
-import { v1 as guid } from 'uuid';
-import { SERVERS } from './servers';
 
-export const token = guid();
-export const clientId = guid();
-export const clientSecret = guid();
-export const refreshToken = guid();
-export const code = guid();
-export const state = guid();
+export const token = "a2a3fa68-658a-4916-94ad-f1170a649839";
+export const clientId = "10ae9344-e599-4c49-ab0c-2891521ccc46";
+export const clientSecret = "013e6077-5ef1-4679-b353-a5792f9faabf";
+export const refreshToken = "8681bade-6c3a-4b86-a38a-378da2bf1181";
+export const code = "3bd5b7a0-876b-4a06-9aa8-903fe4d7e8e6";
+export const state = "54719797-8ee7-488c-824a-f40fd9fab348";
 
-export const authorizationUrl = `${SERVERS.SECURE}/connect/authorize`;
-export const redirectUri = `${SERVERS.API}/oauth2/callback`;
+export const authorizationUrl = "https://secure.counter-culture.io/connect/authorize";
+export const redirectUri = "https://secure.counter-culture.io/oauth2/callback";
 
 export const counters = [
   {
@@ -34,19 +32,19 @@ export const counters = [
   }
 ];
 export const authRequest = {
-  code: guid(),
+  code,
   redirectUri,
   clientId,
   clientSecret,
   scope: 'openid',
   grantType: 'authorization_code'
-} as AuthorizationRequest
+} as AuthorizationRequest;
 export const refreshTokenRequest = {
   clientId,
   clientSecret,
   grantType: 'authorization_code',
   refreshToken,
-}
+} as AuthorizationRequest;
 export const authResponse = {
   access_token: token,
   expires_in: Date.now.toString(),
